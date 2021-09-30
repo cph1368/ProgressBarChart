bd6c104d91f34bd21450a81a24119b356b486ff5
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-09-30 16:50:35 +1000

    Fix ProgressBar

--- ProgressBarChart/ContentView.swift
+++ ProgressBarChart/ContentView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Rectangle().frame(width: 300, height: 50)
+                .foregroundColor(Color.white)
+            
+            HStack {
+                Rectangle()
+                    .frame(width: 180 , height: 50, alignment: .leading)
+                    .foregroundColor(Color(#colorLiteral(red: 0.8332621455, green: 0.1409429014, blue: 0.06613579392, alpha: 1)))
+                    .cornerRadius(45.0)
+                Spacer()
+                Rectangle()
+                    .frame(width: 120 , height: 50, alignment: .leading)
+                    .foregroundColor(Color(#colorLiteral(red: 0.1347649097, green: 0.1234534159, blue: 0.3545898795, alpha: 1)))
+                    .cornerRadius(45.0)
+            }
+            
+          
+            
+        }.cornerRadius(45.0)
             .padding()
     }
 }
