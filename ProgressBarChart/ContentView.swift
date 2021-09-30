0bbc95c5e42be75c7aaa589ddc535a1f0941ee7b
cph1368 <31093206+cph1368@users.noreply.github.com>
2021-09-30 17:06:42 +1000

    Fix Chart model

--- ProgressBarChart/ContentView.swift
+++ ProgressBarChart/ContentView.swift
@@ -9,26 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Rectangle().frame(width: 300, height: 50)
+        VStack(alignment: .leading) {
+            ZStack {
+                Rectangle().frame(width: 300, height: 50)
                 .foregroundColor(Color.white)
-            
-            HStack {
-                Rectangle()
-                    .frame(width: 180 , height: 50, alignment: .leading)
-                    .foregroundColor(Color(#colorLiteral(red: 0.8332621455, green: 0.1409429014, blue: 0.06613579392, alpha: 1)))
-                    .cornerRadius(45.0)
-                Spacer()
-                Rectangle()
-                    .frame(width: 120 , height: 50, alignment: .leading)
-                    .foregroundColor(Color(#colorLiteral(red: 0.1347649097, green: 0.1234534159, blue: 0.3545898795, alpha: 1)))
-                    .cornerRadius(45.0)
+                
+                HStack {
+
+                    RoundedCornersShape(corners: [.topLeft, .bottomLeft], radius: 15)
+                        .fill(Color(#colorLiteral(red: 0.8202568293, green: 0.1505224109, blue: 0.09123057872, alpha: 1)))
+                        .frame(width: 200, height: 20)
+                    Spacer()
+              
+                    RoundedCornersShape(corners: [.topRight, .bottomRight], radius: 15)
+                        .fill(Color(#colorLiteral(red: 0.1263336241, green: 0.1195754185, blue: 0.3506284952, alpha: 1)))
+                        .frame(width: 150, height: 20)
+                }
+                
+               
             }
             
-          
-            
-        }.cornerRadius(45.0)
+            VStack(alignment: .leading) {
+                Text("Spending")
+                Text("4,246.83")
+            }
+            .frame(width: 350)
             .padding()
+        }
     }
 }
 
@@ -37,3 +44,17 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct RoundedCornersShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
